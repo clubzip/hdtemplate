17e9ef0857762fd5e2d55b010e3c1b55b4e56e19
ecourse <smkang@codenuri.co.kr>
2023-07-18 09:30:26 +0900

    Add files via upload

--- DAY/1_basic1.cpp
+++ DAY/1_basic1.cpp
@@ -1,7 +1,7 @@
 // 5 page
-
 // 함수 오버로딩 : 인자의 갯수나 타입이 다르면 동일이름의 함수를 여러개 만들수 있다.
 
+/*
 int square(int a)
 {
 	return a * a;
@@ -11,21 +11,29 @@ double square(double a)
 {
 	return a * a;
 }
+*/
 
+// 함수의 구현이 동일(유사)하다면 함수를 여러개 만들지 말고
+// 함수를 만드는 "틀(템플릿)"을 제공합니다
+// 아래 코드는 "함수"가 아니라 "함수를 만드는 틀" 입니다.
+template<typename T> 
+T square(T a)
+{
+	return a * a;
+}
 int main()
 {
+	// 함수 템플릿(틀)을 사용하는 정확한 코드
+	square<int>(3);		// 1. 컴파일 시간에 필요한 함수생성
+						//    int square(int) 함수를 생성
+						// 2. 이 위치는 생성된 함수를 호출하는 기계어 생성
+						//	  call square<int>(3) 
+	square<double>(3.3);// 1. double square(double) 함수 생성
+						// 2. call square<double>(3.3) 
+
+	// 함수 템플릿 사용시 타입 인자를 생성하면
+	// => 함수 인자를 보고 타입을 결정
+	square(3);	// 3을 보고 T=int 로 결정
+	square(3.3);// 3.3 을 보고 T= double 로 결정
 }
-
-
-// visual studio 사용시 => visualstudio.net
-
-// 1. 프로젝트 버전 설정해야 합니다.
-// => 프로젝트 메뉴, 속성 메뉴 선택
-
-// 2. 파일을 빌드에서 포함/제외 하는 방법
-
-// 3. 빌드하는 방법 : Ctrl + F5
-
-// g++ 사용시 
-// g++ 소스이름.cpp -std=c++20 ( 필요한 경우만, 생략시 C++14)
 
