c24cd782db85f59b700a2f20ad7169f28381a4a2
ecourse <smkang@codenuri.co.kr>
2023-07-18 14:50:29 +0900

    Add files via upload

--- DAY1/4_instantiation8-1.cpp
+++ DAY1/4_instantiation8-1.cpp
@@ -17,6 +17,17 @@ void f2(T a, typename std::type_identity<T>::type b)
 int main()
 {
 //	foo(3); // error
-	f1(3, 3.3);
-	f2(3, 3.3);
+	
+	f1(3, 3.3); // error. 타입 인자가 생략되었으므로
+				// 함수 인자로 타입을 추론해야 하는데.
+				// 1번째 인자로 추론하면 : T=int
+				// 2번째 인자로 추론하면 : T=double
+				// 따라서, 컴파일러는 타입 추론 못합니다. error
+
+	f2(3, 3.3); // 1번째 인자로 T=int 로 추론
+				// 2번째 인자는 "std::type_identity<int>::type" 이므로
+				// f2(int, int) 인데. 3.3 은 int로 전달 가능
+
+				// 즉, f2는 2번째 인자의 타입의 조건은
+				// 1번째 인자로 변환가능한 타입이면 ok.!
 }
