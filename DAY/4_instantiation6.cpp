8d82da88bf13063b7895257db58ba2e7c904849b
ecourse <smkang@codenuri.co.kr>
2023-07-18 13:49:56 +0900

    Add files via upload

--- DAY/4_instantiation6.cpp
+++ DAY/4_instantiation6.cpp
@@ -1,7 +1,6 @@
 #include <vector>
 #include <list>
 
-
 template<typename T>
 class List
 {
@@ -12,6 +11,9 @@ public:
 	// STL 컨테이너를 받을수 있는 생성자
 	template<typename C>
 	List(const C& c) {}
+
+	template<typename IT>
+	List(IT first, IT last) {}
 };
 
 // C++17 "class template type deduction guide" 문법
@@ -22,8 +24,10 @@ List() -> List<int>;		// 디폴트 생성자 사용시는 T=int로 해달라
 							// 물론, 이경우는 에러가 더 좋습니다.
 
 template<typename C>		// 현재 C = vector<int>
-List(const C& c) -> List< typename C::type >;
+List(const C& c) -> List< typename C::value_type >;
 
+template<typename IT>
+List(IT first, IT last) -> List<typename IT::value_type>;
 
 int main()
 {
@@ -38,8 +42,13 @@ int main()
 	List s5(v); // v 의 타입은 "vector<int>" 입니다.
 				// List<int> 로 결정되어야 합니다.
 				// 즉, vector<int> 가 저장하는 타입으로 결정되어야 합니다.
-				// vector<int>::type 으로 해야 합니다.
+				// vector<int>::value_type 으로 해야 합니다.
 
-//	List s6(v.begin(), v.end());
+	List s6(v.begin(), v.end());
+
+	// C++17 이후에 STL 에 이미 위처럼 많은 
+	// deduction guide 가 제공됩니다.
+	// => 따라서 C++17 이후에는 클래스 템플릿 사용시에
+	// => 템플릿 인자를 생략하는 경우가 많아 집니다.
 }
 
