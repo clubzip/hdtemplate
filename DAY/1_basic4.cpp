86ed8ba92c605e703445eb6df5f7e0e1c193adc9
ecourse <smkang@codenuri.co.kr>
2023-07-18 10:32:45 +0900

    Add files via upload

--- DAY/1_basic4.cpp
+++ DAY/1_basic4.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <source_location> // C++20 에서 추가된 헤더.
 
+#include "fname.h"
+
+// fname.h : C++20 지원 안되고 include 가능합니다(조건부 컴파일 사용)
+// C++20 지원 되면 : function_name() 사용
+//         안되면 : _FNAME_ 매크로 사용
+
 // 파일, 함수의 이름을 얻는 방법
 // C 언어 : __FILE__, __LINE__, __func__ 등의 매크로 사용
 // C++20 : std::source_location
@@ -8,12 +14,17 @@
 template<typename T>
 T square(T a)
 {
-	std::source_location loc = std::source_location::current();
+	std::string s = function_name(); // "fname.h" 에 있는 함수
+				 // function_name(std::source_location::current());
 
+	std::cout << s << std::endl;
+
+	/*
+	std::source_location loc = std::source_location::current();
 	std::cout << loc.file_name() << std::endl;
 	std::cout << loc.line()      << std::endl;
 	std::cout << loc.function_name() << std::endl;
-
+	*/
 	return a * a;
 }
 
