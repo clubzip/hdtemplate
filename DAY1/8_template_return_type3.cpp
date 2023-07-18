7c9c775c2225a9af635a39c3e5d31478c39720cc
ecourse <smkang@codenuri.co.kr>
2023-07-18 16:16:46 +0900

    Add files via upload

--- DAY1/8_template_return_type3.cpp
+++ DAY1/8_template_return_type3.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 
+// 방법 1, 2 : 2개의 인자를 같은 타입!!!
+// 
 // 방법 3. 2개의 인자를 서로 다른 타입이 되도록
-
+// => 이 경우의 핵심은 "반환 타입을 어떻게 표기할것인가?" 입니다.
+// => 여러가지 방법이 있습니다.
 template<typename T1, typename T2>
-? add(const T1& a, const T2& b)
+? add(const T1 & a, const T2 & b)
 {
 	return a + b;
 }
