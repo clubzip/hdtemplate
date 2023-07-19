f765ed1dd44b944358c80d7a88de4b6113c4ddb3
ecourse <smkang@codenuri.co.kr>
2023-07-19 10:44:54 +0900

    Add files via upload

--- DAY2/2_type_deduction8.cpp
+++ DAY2/2_type_deduction8.cpp
@@ -2,22 +2,20 @@ template<typename T>
 void f1(T a)
 {
 }
-
 template<typename T>
 void f2(T& a)
 {
 }
-
 int main()
 {
 	int x[3] = { 1,2,3 };
 	int y[5] = { 1,2,3, 4, 5 };
 
-	f1(x);
-	f1(y);
-	f2(x);
-	f2(y);
+	f1(x); // f1(int* a)
+	f1(y); // f1(int* a)
+	f2(x); // f2(int(&a)[3])
+	f2(y); // f2(int(&a)[5])
 }
 // 위 코드를 컴파일 했을때 함수가 몇개 생성될지 예측해 보세요
-// f1 : ? 개
-// f2 : ? 개
+// f1 : 1 개
+// f2 : 2 개
