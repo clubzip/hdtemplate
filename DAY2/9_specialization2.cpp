3cac86b144d8f64ddf8eb5e8b93e38ea117d4a11
ecourse <smkang@codenuri.co.kr>
2023-07-19 16:29:26 +0900

    Add files via upload

--- DAY2/9_specialization2.cpp
+++ DAY2/9_specialization2.cpp
@@ -2,6 +2,7 @@
 
 // STL 의 vector 구현에 대해서 생각해 봅시다.
 
+// "primary template"
 template<typename T> class Vector
 {
 	T* ptr;
@@ -15,8 +16,10 @@ public:
 	~Vector() { delete[] ptr; }
 };
 
-// template specialization
+// template specialization(특수화, 전문화)
 // => template 구현시, 특정 타입에 대해서는 내부 구현을 변경할때 사용
+// => 타입이 완전하게 결정된 경우. 
+// => <> 안에 typename 이 없습니다.
 template<> class Vector<bool>
 {
 	int* ptr;
@@ -33,6 +36,21 @@ public:
 };
 
 
+// T가 임의 타입의 포인터 일때
+// template partial specialization(부분 특수화, 부분 전문화)
+// => <> 안에 typename 이 있습니다.
+template<typename T> class Vector<T*>
+{
+	T* ptr;
+	std::size_t size;
+public:
+	Vector(std::size_t sz) : size(sz)
+	{
+		ptr = new T[sz];
+		std::cout << "Vector<T*>" << std::endl;
+	}
+	~Vector() { delete[] ptr; }
+};
 
 int main()
 {
@@ -40,5 +58,6 @@ int main()
 	Vector<double> v2(5);   
 
 	Vector<bool>   v3(100); 
+	Vector<int*>   v4(5);
 }
 
