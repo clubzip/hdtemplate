52bbd0d5b38ab94c07e7c96035edcd06f6d24718
ecourse <smkang@codenuri.co.kr>
2023-07-20 10:38:35 +0900

    Add files via upload

--- DAY3/move_구현.cpp
+++ DAY3/move_구현.cpp
@@ -8,9 +8,12 @@ public:
 	String(const String& s) { std::cout << "copy ctor" << std::endl; }
 	String(String&& s)      { std::cout << "move ctor" << std::endl; }
 };
+
 int main()
 {
 	String s1 = "hello";
 	String s2 = s1; // copy
 	String s3 = std::move(s1); // move
+	String s4 = static_cast<String&&>(s2); 
+				// std::move 는 결국 위코드 처럼 rvalue 캐스팅입니다.
 }
