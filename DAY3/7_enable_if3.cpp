64d0bc02a203edc5f456d2453ee141ad93e1dddd
ecourse <smkang@codenuri.co.kr>
2023-07-20 14:44:05 +0900

    Add files via upload

--- DAY3/7_enable_if3.cpp
+++ DAY3/7_enable_if3.cpp
@@ -5,9 +5,11 @@
 // std::enable_if<bool값, 타입>::type    의 의미를 생각해 보세요
 
 template<typename T> 
-
-std::enable_if< std::is_integral_v<T>, T>::type
-
+						// T가 정수라면 true     ::type 는 T, 즉 반환타입은 T
+typename std::enable_if< std::is_integral_v<T>, T>::type
+						// T가 정수가 아니면 false  ::type 가 없다. 
+						// 즉, 함수 생성 실패, 하지만 SFINAE 규칙 때문에
+						// 에러가 아니고 후보에서 제외!
 gcd(T a, T b)
 {
 	return 0;
