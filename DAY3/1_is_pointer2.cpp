f5079d07583d84940148d9638b557203e9db3eac
ecourse <smkang@codenuri.co.kr>
2023-07-20 09:08:45 +0900

    Add files via upload

---
+++ DAY3/1_is_pointer2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+// 아래 코드가 T 가 포인터 인지 조사하는 기술 입니다.
+template<typename T>
+struct is_pointer
+{
+	enum { value = false };
+};
+
+
+template<typename T> 
+void fn(const T& arg)
+{
+	// 현재 T는 int, int* 
+	if (  is_pointer<T>::value  )
+		std::cout << "pointer" << std::endl;
+	else
+		std::cout << "not pointer" << std::endl;
+}
+
+int main()
+{
+	int n = 0;
+	fn(n);
+	fn(&n);
+}
