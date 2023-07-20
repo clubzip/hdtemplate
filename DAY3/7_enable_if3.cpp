5758e8da194b53e4c48dac75ea83d723d5447639
ecourse <smkang@codenuri.co.kr>
2023-07-20 14:40:00 +0900

    Add files via upload

---
+++ DAY3/7_enable_if3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <type_traits>
+
+
+// std::enable_if<bool값, 타입>::type    의 의미를 생각해 보세요
+
+template<typename T> 
+
+std::enable_if< std::is_integral_v<T>, T>::type
+
+gcd(T a, T b)
+{
+	return 0;
+}
+
+
+
+
+
+
+double gcd(double a, double b)
+{
+	std::cout << "double" << std::endl;
+	return 0;
+}
+
+int main()
+{
+	gcd(6, 9);
+
+	gcd(3.1, 5.4);		
+	gcd(3.1f, 5.4f);	
+}
