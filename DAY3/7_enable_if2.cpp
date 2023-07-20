5b8bb9ce9c1534d9b42d18c7cf21e5eb1a291497
ecourse <smkang@codenuri.co.kr>
2023-07-20 14:36:44 +0900

    Add files via upload

--- DAY3/7_enable_if2.cpp
+++ DAY3/7_enable_if2.cpp
@@ -7,7 +7,6 @@ template<typename T> T gcd(T a, T b)
 	static_assert(std::is_integral_v<T>, "Error T is not integer");
 	return 0; 
 }
-
 double gcd(double a, double b)
 {
 	std::cout << "double" << std::endl;
@@ -18,5 +17,11 @@ int main()
 {
 	gcd(6, 9);
 
-	gcd(3.1,  5.4);		
+	gcd(3.1,  5.4);		// double 버전 사용
+	gcd(3.1f, 5.4f);	// T 버전 사용. static_assert 가 실패하므로
+						// 컴파일 에러!!
+
+	// 핵심
+	// static_assert(조건) : 조건을 만족하지 않으면 컴파일 에러
+	// => 조건을 만족하지 않으면 에러를 내지 말고, 후보에서 제외될수 없을까?
 }
