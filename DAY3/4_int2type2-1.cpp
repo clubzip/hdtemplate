cf9e8548d4a07b5b06f9d919a97df8486499b4ef
ecourse <smkang@codenuri.co.kr>
2023-07-20 13:08:24 +0900

    Add files via upload

--- DAY3/4_int2type2-1.cpp
+++ DAY3/4_int2type2-1.cpp
@@ -18,7 +18,9 @@ template<typename T>
 void printv(const T& value)
 {
 	// 함수 오버로딩을 사용해서 조건에 따라서 "다른 함수"를 호출합니다.
-	printv_imp(value, ? );
+	printv_imp(value, std::is_pointer<T>::value  );
+						// T 포인터라면   : true 즉, 1
+						// T 포인터아니면 : false 즉, 0
 }
 
 
