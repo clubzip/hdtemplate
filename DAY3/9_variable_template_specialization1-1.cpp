4640ca97cb83487fe3b6188088a003ce9bc714fc
ecourse <smkang@codenuri.co.kr>
2023-07-20 16:25:39 +0900

    Add files via upload

--- DAY3/9_variable_template_specialization1-1.cpp
+++ DAY3/9_variable_template_specialization1-1.cpp
@@ -17,9 +17,9 @@ constexpr int made_year<Sample> = 2023;
 
 int main()
 {
-	std::cout << made_year<int> << std::endl;	
-	std::cout << made_year<double> << std::endl;
-	std::cout << made_year<Sample> << std::endl;
+	std::cout << made_year<int> << std::endl;	 // -1
+	std::cout << made_year<double> << std::endl; // -1
+	std::cout << made_year<Sample> << std::endl; // 2023
 
 }
 
