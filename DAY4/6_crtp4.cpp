6412aeebdf75cde2436e2a6c6e3f096305e32325
ecourse <smkang@codenuri.co.kr>
2023-07-21 15:20:27 +0900

    Add files via upload

--- DAY4/6_crtp4.cpp
+++ DAY4/6_crtp4.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
+// 아래 처럼 만들면 10개의 파생 클래스를 만들게 될때
+// 10개의 기반 클래스가 모두 다른 클래스 입니다.
+// 멤버 함수가 20개 라면 (20 * 10개 클래스 => 200개 함수 생성)
+
+// class W1 : public Window<W1>{}
+// class W2 : public Window<W2>{}
+// class W3 : public Window<W3>{}
+
+// std::vector<기반클래스???*> v; // 기반 클래스가 모두 다릅니다.!!
+
 
 template<typename T>
 class Window
@@ -28,6 +38,13 @@ class MainWindow : public Window<MainWindow>
 public:
 	void Click() { std::cout << "MainWindow Click" << std::endl; }
 };
+class ImageView : public Window<ImageView>
+{
+public:
+	void Click() { std::cout << "MainWindow Click" << std::endl; }
+};
+
+
 int main()
 {
 	MainWindow w;
